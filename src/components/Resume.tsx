8882eda4a6beec719d70283c8120099d69f49b9e
Philipp Knoche <130101659+pknoche@users.noreply.github.com>
2024-06-28 21:34:08 -0500

    Replace contact section with resume

---
+++ src/components/Resume.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Resume: React.FC = () => {
+  return (
+    <section id="resume" className="mb-8">
+      <h2 className="mb-8">Resume</h2>
+      <div className="bg-white p-4 rounded shadow-md">
+        <iframe
+          src="/Philipp_Knoche_resume.pdf"
+          className="w-full h-[1125px]"
+          title="Resume"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Resume;
