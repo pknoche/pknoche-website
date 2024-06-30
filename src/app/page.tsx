afd26f0ae2e600a52a227ea621294d77359c11d3
Philipp Knoche <130101659+pknoche@users.noreply.github.com>
2024-06-30 16:48:21 -0500

    Move components directory to app directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import About from '../components/About';
-import Resume from '../components/Resume';
-import Projects from '../components/Projects';
-import UnderConstruction from '../components/UnderConstruction';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './components/About';
+import Resume from './components/Resume';
+import Projects from './components/Projects';
+import UnderConstruction from './components/UnderConstruction';
 import './globals.css';
 
 const Home: React.FC = () => {
