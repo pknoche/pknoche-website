09ad6a77e13586219e08ae5e059b6d4ac6ad596a
Philipp Knoche <130101659+pknoche@users.noreply.github.com>
2024-06-30 23:41:34 -0500

    Replace project placeholders with project information

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -3,45 +3,45 @@ import Image from 'next/image';
 
 const projectData = [
   {
-    src: '/placeholder_image.jpg',
-    alt: 'Placeholder Image 1',
-    title: 'Project Title 1',
-    description: 'This is a placeholder description for project 1.',
-    url: 'https://www.github.com/pknoche/',
+    src: '/portfolio-website.jpg',
+    alt: 'briefcase',
+    title: 'Portfolio Website',
+    description: 'Implemented with React and Next.js, this website is hosted on AWS and serves as a platform to showcase my portfolio and try out new technologies and projects.',
+    url: 'https://www.github.com/pknoche/pknoche-website',
   },
   {
-    src: '/placeholder_image.jpg',
-    alt: 'Placeholder Image 2',
-    title: 'Project Title 2',
-    description: 'This is a placeholder description for project 2.',
-    url: 'https://www.github.com/pknoche/',
+    src: '/employee-attrition-predictor.png',
+    alt: 'employee attrition prediction system screenshot',
+    title: 'Employee Attrition Prediction System',
+    description: 'Implemented with Jupyter Notebook and Python, this project uses machine learning to predict employee attrition based on various factors.',
+    url: 'https://github.com/pknoche/employee-attrition-predictor',
   },
   {
-    src: '/placeholder_image.jpg',
-    alt: 'Placeholder Image 3',
-    title: 'Project Title 3',
-    description: 'This is a placeholder description for project 3.',
-    url: 'https://www.github.com/pknoche/',
+    src: '/package-routing-program.jpg',
+    alt: 'packages on truck',
+    title: 'Package Routing Program',
+    description: 'Implemented with Python, this program simulates a package routing system for a delivery company. It uses a number of heuristic algorithms to determine the most efficient delivery routes and recommends package loading order.',
+    url: 'https://www.github.com/pknoche/package-routing-program',
   },
   {
-    src: '/placeholder_image.jpg',
-    alt: 'Placeholder Image 4',
-    title: 'Project Title 4',
-    description: 'This is a placeholder description for project 4.',
-    url: 'https://www.github.com/pknoche/',
+    src: '/appointment-manager.jpg',
+    alt: 'calendar',
+    title: 'Appointment Manager',
+    description: 'Implemented with Java and JavaFX, this program allows users to schedule appointments, manage customer contact information, and generate reports. It uses a MySQL database to store appointment and customer information.',
+    url: 'https://www.github.com/pknoche/appointment-manager',
   },
   {
-    src: '/placeholder_image.jpg',
-    alt: 'Placeholder Image 5',
-    title: 'Project Title 5',
-    description: 'This is a placeholder description for project 5.',
-    url: 'https://www.github.com/pknoche/',
+    src: '/inventory-management-system.jpg',
+    alt: 'items on shelf in warehouse',
+    title: 'Inventory Management System',
+    description: 'Implemented with Java and JavaFX, this program allows users to add and delete inventory, assign parts to products, and search for parts and products.',
+    url: 'https://www.github.com/pknoche/inventory-management-system',
   },
   {
-    src: '/placeholder_image.jpg',
-    alt: 'Placeholder Image 6',
-    title: 'Project Title 6',
-    description: 'This is a placeholder description for project 6.',
+    src: '/student-roster-program.jpg',
+    alt: 'stack of books',
+    title: 'Student Roster Program',
+    description: 'Implemented with C++, this program parses student data from a csv file and allows users to add, delete, and search for students and generate reports.',
     url: 'https://www.github.com/pknoche/',
   },
 ];
@@ -53,13 +53,12 @@ const Projects: React.FC = () => {
       <div className='grid grid-cols-3 gap-4'>
         {projectData.map((project, index) => (
           <div key={index} className='card bg-base-100 shadow-xl'>
-            <figure>
+            <figure className='flex h-48 items-center justify-center overflow-hidden'>
               <Image
                 src={project.src}
                 alt={project.alt}
                 width={500}
                 height={300}
-                layout='responsive'
               />
             </figure>
             <div className='card-body'>
