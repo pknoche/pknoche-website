b065c4d5b8dbe09c7e42c40fa6b1d5d7462b6e5a
Philipp Knoche <130101659+pknoche@users.noreply.github.com>
2024-06-30 23:45:46 -0500

    Format code with Prettier

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -6,42 +6,48 @@ const projectData = [
     src: '/portfolio-website.jpg',
     alt: 'briefcase',
     title: 'Portfolio Website',
-    description: 'Implemented with React and Next.js, this website is hosted on AWS and serves as a platform to showcase my portfolio and try out new technologies and projects.',
+    description:
+      'Implemented with React and Next.js, this website is hosted on AWS and serves as a platform to showcase my portfolio and try out new technologies and projects.',
     url: 'https://www.github.com/pknoche/pknoche-website',
   },
   {
     src: '/employee-attrition-predictor.png',
     alt: 'employee attrition prediction system screenshot',
     title: 'Employee Attrition Prediction System',
-    description: 'Implemented with Jupyter Notebook and Python, this project uses machine learning to predict employee attrition based on various factors.',
+    description:
+      'Implemented with Jupyter Notebook and Python, this project uses machine learning to predict employee attrition based on various factors.',
     url: 'https://github.com/pknoche/employee-attrition-predictor',
   },
   {
     src: '/package-routing-program.jpg',
     alt: 'packages on truck',
     title: 'Package Routing Program',
-    description: 'Implemented with Python, this program simulates a package routing system for a delivery company. It uses a number of heuristic algorithms to determine the most efficient delivery routes and recommends package loading order.',
+    description:
+      'Implemented with Python, this program simulates a package routing system for a delivery company. It uses a number of heuristic algorithms to determine the most efficient delivery routes and recommends package loading order.',
     url: 'https://www.github.com/pknoche/package-routing-program',
   },
   {
     src: '/appointment-manager.jpg',
     alt: 'calendar',
     title: 'Appointment Manager',
-    description: 'Implemented with Java and JavaFX, this program allows users to schedule appointments, manage customer contact information, and generate reports. It uses a MySQL database to store appointment and customer information.',
+    description:
+      'Implemented with Java and JavaFX, this program allows users to schedule appointments, manage customer contact information, and generate reports. It uses a MySQL database to store appointment and customer information.',
     url: 'https://www.github.com/pknoche/appointment-manager',
   },
   {
     src: '/inventory-management-system.jpg',
     alt: 'items on shelf in warehouse',
     title: 'Inventory Management System',
-    description: 'Implemented with Java and JavaFX, this program allows users to add and delete inventory, assign parts to products, and search for parts and products.',
+    description:
+      'Implemented with Java and JavaFX, this program allows users to add and delete inventory, assign parts to products, and search for parts and products.',
     url: 'https://www.github.com/pknoche/inventory-management-system',
   },
   {
     src: '/student-roster-program.jpg',
     alt: 'stack of books',
     title: 'Student Roster Program',
-    description: 'Implemented with C++, this program parses student data from a csv file and allows users to add, delete, and search for students and generate reports.',
+    description:
+      'Implemented with C++, this program parses student data from a csv file and allows users to add, delete, and search for students and generate reports.',
     url: 'https://www.github.com/pknoche/',
   },
 ];
