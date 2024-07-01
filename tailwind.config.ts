37967a93620cc8ff15018fa3871eeceebd266339
Philipp Knoche <130101659+pknoche@users.noreply.github.com>
2024-06-30 20:44:05 -0500

    Remove old config comments

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,22 +18,3 @@ const config: Config = {
 };
 
 export default config;
-
-// const config: Config = {
-//   content: [
-//     "./src/app/**/*.{js,ts,jsx,tsx}",
-//     "./src/components/**/*.{js,ts,jsx,tsx}",
-//     "./src/pages/**/*.{js,ts,jsx,tsx}",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [
-//     require('daisyui')
-//   ],
-//   daisyui: [
-//     themes: ["light"],
-//   ],
-// };
-
-// export default config;
