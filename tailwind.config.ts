f33274919632cd577f9f9fd9a1284f112b1e584f
Philipp Knoche <130101659+pknoche@users.noreply.github.com>
2024-06-30 20:38:32 -0500

    Add daisyUI theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,28 @@ const config: Config = {
   plugins: [
     require('daisyui')
   ],
+  daisyui: {
+    themes: ["light"],
+  },
 };
 
 export default config;
+
+// const config: Config = {
+//   content: [
+//     "./src/app/**/*.{js,ts,jsx,tsx}",
+//     "./src/components/**/*.{js,ts,jsx,tsx}",
+//     "./src/pages/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     require('daisyui')
+//   ],
+//   daisyui: [
+//     themes: ["light"],
+//   ],
+// };
+
+// export default config;
