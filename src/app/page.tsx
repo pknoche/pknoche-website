f17054b4192cd5f0e92353fb004b665f0ebef56b
Philipp Knoche <130101659+pknoche@users.noreply.github.com>
2024-07-01 13:35:02 -0500

    Remove under construction banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,13 @@ import Footer from './components/Footer';
 import About from './components/About';
 import Resume from './components/Resume';
 import Projects from './components/Projects';
-import UnderConstruction from './components/UnderConstruction';
+// import UnderConstruction from './components/UnderConstruction';
 import './globals.css';
 
 const Home: React.FC = () => {
   return (
     <div className='flex min-h-screen flex-col'>
-      <UnderConstruction />
+      {/* <UnderConstruction /> */}
       <Header />
       <main className='container mx-auto flex-grow space-y-8 p-4'>
         <About />
